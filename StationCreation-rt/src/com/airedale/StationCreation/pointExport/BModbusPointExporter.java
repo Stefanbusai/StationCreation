8fde738015b93b831b2291e6df359768d6677bfc
Phil Holden <Philip.A.Holden@airedale.com>
2025-06-20 13:24:26 +0100

    New BModbusPointExporter

---
+++ StationCreation-rt/src/com/airedale/StationCreation/pointExport/BModbusPointExporter.java
@@ -0,0 +1,332 @@
+package com.airedale.StationCreation.pointExport;
+
+import com.airedale.StationCreation.utils.BAcisWorker;
+import com.airedale.StationCreation.utils.FileUtils;
+import com.airedale.StationCreation.utils.StringUtils;
+import com.tridium.modbusCore.BModbusDevice;
+import com.tridium.modbusCore.server.point.BModbusServerPointDeviceExt;
+import com.tridium.modbusCore.server.point.BModbusServerProxyExt;
+
+import javax.baja.bacnet.export.BBacnetPointDescriptor;
+import javax.baja.control.BControlPoint;
+import javax.baja.naming.BOrd;
+import javax.baja.nre.annotations.NiagaraAction;
+import javax.baja.nre.annotations.NiagaraProperty;
+import javax.baja.nre.annotations.NiagaraType;
+import javax.baja.status.BStatus;
+import javax.baja.status.BStatusString;
+import javax.baja.sys.*;
+import javax.baja.util.BFormat;
+import javax.baja.util.IFuture;
+import javax.baja.util.Invocation;
+import java.util.List;
+import java.util.logging.Logger;
+
+@NiagaraType
+
+@NiagaraProperty(
+        name = "status",
+        type = "BStatusString",
+        defaultValue = "new BStatusString(\"\")",
+        flags = Flags.SUMMARY | Flags.READONLY)
+
+@NiagaraProperty(
+        name = "modbusSlaveDeviceOrd",
+        type = "BOrd",
+        defaultValue = "BOrd.make(\"station:|slot:/Drivers/ModbusSlaveNetwork/ModbusSlaveDevice\")",
+        flags = Flags.SUMMARY)
+
+@NiagaraProperty(
+        name = "pointNameFormat",
+        type = "BFormat",
+        defaultValue = "BFormat.make(\"%name%\")",
+        flags = Flags.SUMMARY)
+
+@NiagaraProperty(
+        name = "csvFile",
+        type = "BOrd",
+        defaultValue = "BOrd.make(\"file:^points_to_export_to_bacnet.csv\")",
+        flags = Flags.SUMMARY)
+
+@NiagaraProperty(
+        name = "worker",
+        type = "BAcisWorker",
+        defaultValue = "new BAcisWorker()",
+        flags = Flags.HIDDEN)
+
+@NiagaraAction(
+        name = "exportPoints",
+        flags = Flags.ASYNC)
+
+@NiagaraAction(
+        name = "reset")
+
+public class BModbusPointExporter
+        extends BComponent
+{
+//region /*+ ------------ BEGIN BAJA AUTO GENERATED CODE ------------ +*/
+//@formatter:off
+/*@ $com.airedale.StationCreation.pointExport.BModbusPointExporter(258645149)1.0$ @*/
+/* Generated Fri Jun 20 13:19:24 BST 2025 by Slot-o-Matic (c) Tridium, Inc. 2012-2025 */
+
+  //region Property "status"
+
+  /**
+   * Slot for the {@code status} property.
+   * @see #getStatus
+   * @see #setStatus
+   */
+  public static final Property status = newProperty(Flags.SUMMARY | Flags.READONLY, new BStatusString(""), null);
+
+  /**
+   * Get the {@code status} property.
+   * @see #status
+   */
+  public BStatusString getStatus() { return (BStatusString)get(status); }
+
+  /**
+   * Set the {@code status} property.
+   * @see #status
+   */
+  public void setStatus(BStatusString v) { set(status, v, null); }
+
+  //endregion Property "status"
+
+  //region Property "modbusSlaveDeviceOrd"
+
+  /**
+   * Slot for the {@code modbusSlaveDeviceOrd} property.
+   * @see #getModbusSlaveDeviceOrd
+   * @see #setModbusSlaveDeviceOrd
+   */
+  public static final Property modbusSlaveDeviceOrd = newProperty(Flags.SUMMARY, BOrd.make("station:|slot:/Drivers/ModbusSlaveNetwork/ModbusSlaveDevice"), null);
+
+  /**
+   * Get the {@code modbusSlaveDeviceOrd} property.
+   * @see #modbusSlaveDeviceOrd
+   */
+  public BOrd getModbusSlaveDeviceOrd() { return (BOrd)get(modbusSlaveDeviceOrd); }
+
+  /**
+   * Set the {@code modbusSlaveDeviceOrd} property.
+   * @see #modbusSlaveDeviceOrd
+   */
+  public void setModbusSlaveDeviceOrd(BOrd v) { set(modbusSlaveDeviceOrd, v, null); }
+
+  //endregion Property "modbusSlaveDeviceOrd"
+
+  //region Property "pointNameFormat"
+
+  /**
+   * Slot for the {@code pointNameFormat} property.
+   * @see #getPointNameFormat
+   * @see #setPointNameFormat
+   */
+  public static final Property pointNameFormat = newProperty(Flags.SUMMARY, BFormat.make("%name%"), null);
+
+  /**
+   * Get the {@code pointNameFormat} property.
+   * @see #pointNameFormat
+   */
+  public BFormat getPointNameFormat() { return (BFormat)get(pointNameFormat); }
+
+  /**
+   * Set the {@code pointNameFormat} property.
+   * @see #pointNameFormat
+   */
+  public void setPointNameFormat(BFormat v) { set(pointNameFormat, v, null); }
+
+  //endregion Property "pointNameFormat"
+
+  //region Property "csvFile"
+
+  /**
+   * Slot for the {@code csvFile} property.
+   * @see #getCsvFile
+   * @see #setCsvFile
+   */
+  public static final Property csvFile = newProperty(Flags.SUMMARY, BOrd.make("file:^points_to_export_to_bacnet.csv"), null);
+
+  /**
+   * Get the {@code csvFile} property.
+   * @see #csvFile
+   */
+  public BOrd getCsvFile() { return (BOrd)get(csvFile); }
+
+  /**
+   * Set the {@code csvFile} property.
+   * @see #csvFile
+   */
+  public void setCsvFile(BOrd v) { set(csvFile, v, null); }
+
+  //endregion Property "csvFile"
+
+  //region Property "worker"
+
+  /**
+   * Slot for the {@code worker} property.
+   * @see #getWorker
+   * @see #setWorker
+   */
+  public static final Property worker = newProperty(Flags.HIDDEN, new BAcisWorker(), null);
+
+  /**
+   * Get the {@code worker} property.
+   * @see #worker
+   */
+  public BAcisWorker getWorker() { return (BAcisWorker)get(worker); }
+
+  /**
+   * Set the {@code worker} property.
+   * @see #worker
+   */
+  public void setWorker(BAcisWorker v) { set(worker, v, null); }
+
+  //endregion Property "worker"
+
+  //region Action "exportPoints"
+
+  /**
+   * Slot for the {@code exportPoints} action.
+   * @see #exportPoints()
+   */
+  public static final Action exportPoints = newAction(Flags.ASYNC, null);
+
+  /**
+   * Invoke the {@code exportPoints} action.
+   * @see #exportPoints
+   */
+  public void exportPoints() { invoke(exportPoints, null, null); }
+
+  //endregion Action "exportPoints"
+
+  //region Action "reset"
+
+  /**
+   * Slot for the {@code reset} action.
+   * @see #reset()
+   */
+  public static final Action reset = newAction(0, null);
+
+  /**
+   * Invoke the {@code reset} action.
+   * @see #reset
+   */
+  public void reset() { invoke(reset, null, null); }
+
+  //endregion Action "reset"
+
+  //region Type
+
+  @Override
+  public Type getType() { return TYPE; }
+  public static final Type TYPE = Sys.loadType(BModbusPointExporter.class);
+
+  //endregion Type
+
+//@formatter:on
+//endregion /*+ ------------ END BAJA AUTO GENERATED CODE -------------- +*/
+
+    private static final Logger logger = Logger.getLogger(TYPE.getModule().getModuleName() + "." + TYPE.getTypeName());
+
+    /**
+     * Called by the framework when the component is started.
+     */
+    @Override
+    public void started() throws Exception
+    {
+        // If the station is already at steady state when the
+        // component is started, invoke the atSteadyState() callback.
+        if (Sys.atSteadyState())
+        {
+            atSteadyState();
+        }
+    }
+
+    /**
+     * Called by the framework during station bootstrap, after the steady state timeout has expired.
+     */
+    @Override
+    public void atSteadyState() throws Exception
+    {
+        reset();
+    }
+
+    /**
+     * Called by the framework when the component is stopped.
+     */
+    @Override
+    public void stopped() throws Exception
+    {
+        // do nothing
+    }
+
+    /**
+     * Reset action.
+     */
+    public void doReset()
+    {
+        setStatus(new BStatusString("Ready", BStatus.ok));
+    }
+
+    /**
+     * ExportPoints action.
+     */
+    public void doExportPoints(Context cx){
+        logger.info("Starting export points creation");
+        setStatus(new BStatusString("Starting export", BStatus.ok));
+
+        List<String> listOfPointLinesToExport = FileUtils.readLinesFromFileAsArrayList(getCsvFile());
+        listOfPointLinesToExport.remove(0);
+        for (String pointLineToExport : listOfPointLinesToExport){
+            PointToExport pointToExport = new PointToExport(pointLineToExport);
+            if (pointToExport.isValid()){
+                logger.info("Processing point line: " + pointLineToExport);
+                processExportPoint(pointToExport, cx);
+            }
+        }
+
+        setStatus(new BStatusString("Ready", BStatus.ok));
+    }
+
+    /**
+     * Process an export point.
+     */
+    private void processExportPoint(PointToExport pointToExport, Context cx) {
+        BComponent sourceComponent = (BComponent) pointToExport.getSourceOrd().resolve(Sys.getStation()).get();
+        String pointName = createFormattedPointName(sourceComponent, cx);
+
+        logger.info("Creating new export point with name: " + pointName);
+
+        BControlPoint controlPoint = pointToExport.createModbusServerControlPoint(pointName, cx);
+        if (controlPoint != null) {
+            BOrd modbusServerPointDeviceExtOrd = BOrd.make(getModbusSlaveDeviceOrd().encodeToString() + "/points");
+            BModbusServerPointDeviceExt modbusServerPointDeviceExt = (BModbusServerPointDeviceExt) modbusServerPointDeviceExtOrd.resolve(Sys.getStation()).get();
+            modbusServerPointDeviceExt.add(pointName, controlPoint);
+            logger.info("Created: " + controlPoint.toString(cx));
+        }
+    }
+
+    /**
+     * Create a formatted point name.
+     */
+    private String createFormattedPointName(BComponent component, Context cx)
+    {
+        String formattedPointName = getPointNameFormat().format(component, cx);
+
+        return StringUtils.insertSpecialCharacters(formattedPointName);
+    }
+
+    /**
+     * Use the custom worker thread for actions that have the "async" flag set.
+     */
+    @Override
+    public IFuture post(Action action,
+                        BValue argument,
+                        Context context)
+    {
+        getWorker().postAsync(new Invocation(this, action, argument, context));
+        return null;
+    }
+
+}
--- StationCreation-rt/src/com/airedale/StationCreation/pointExport/PointToExport.java
+++ StationCreation-rt/src/com/airedale/StationCreation/pointExport/PointToExport.java
@@ -1,5 +1,11 @@
 package com.airedale.StationCreation.pointExport;
 
+import com.tridium.modbusCore.enums.BDataTypeEnum;
+import com.tridium.modbusCore.enums.BRegisterTypeEnum;
+import com.tridium.modbusCore.enums.BStatusTypeEnum;
+import com.tridium.modbusCore.server.point.BModbusServerBooleanProxyExt;
+import com.tridium.modbusCore.server.point.BModbusServerNumericProxyExt;
+
 import javax.baja.bacnet.datatypes.BBacnetObjectIdentifier;
 import javax.baja.bacnet.export.*;
 import javax.baja.control.*;
@@ -8,7 +14,8 @@ import javax.baja.sys.Context;
 import javax.baja.sys.Sys;
 import java.util.logging.Logger;
 
-public class PointToExport {
+public class PointToExport
+{
     protected String csvLine;
     protected BOrd sourceOrd;
     protected int address;
@@ -16,83 +23,117 @@ public class PointToExport {
     protected boolean valid = true;
 
     //getters
-    public boolean isValid() {
+    public boolean isValid()
+    {
         return valid;
     }
-    public BOrd getSourceOrd() {
+
+    public BOrd getSourceOrd()
+    {
         return sourceOrd;
     }
-    public int getAddress() {
+
+    public int getAddress()
+    {
         return address;
     }
-    public boolean isWritable() {
+
+    public boolean isWritable()
+    {
         return writable;
     }
 
 
-    public PointToExport(String csvLine) {
+    public PointToExport(String csvLine)
+    {
         this.csvLine = csvLine;
         String[] pointDetails = csvLine.split(",");
         // check the line length
-        if (pointDetails.length != 3){
+        if (pointDetails.length != 3)
+        {
             logger.warning("point to export details not correct length: " + pointDetails.length);
             valid = false;
             return;
         }
         // parse the source ORD
-        try{
+        try
+        {
             sourceOrd = BOrd.make(pointDetails[0]);
-        } catch (Exception e){
+        }
+        catch (Exception e)
+        {
             logger.warning("point to export ORD not valid: " + csvLine);
             valid = false;
             return;
         }
         // parse the address
-        try{
+        try
+        {
             address = Integer.parseInt(pointDetails[1]);
-        } catch (Exception e){
+        }
+        catch (Exception e)
+        {
             logger.warning("point to export address not valid: " + csvLine);
             valid = false;
             return;
         }
         // parse the writable boolean
-        try{
+        try
+        {
             writable = Boolean.parseBoolean(pointDetails[2]);
-        } catch (Exception e){
+        }
+        catch (Exception e)
+        {
             logger.warning("point to export writable boolean not valid: " + csvLine);
             valid = false;
             return;
         }
     }
 
-    private static final Logger logger = Logger.getLogger("PointToExport");
+    private static final Logger logger = Logger.getLogger("stationCreation.PointToExport");
 
-    public BBacnetPointDescriptor createBacnetPointDescriptor(String pointName, Context cx) {
+    /**
+     * Create a Bacnet point descriptor.
+     */
+    public BBacnetPointDescriptor createBacnetPointDescriptor(String pointName, Context cx)
+    {
         // resolve the point in the SourceORD
         BBacnetPointDescriptor bacnetPointDescriptor;
+
         BControlPoint sourcePoint = (BControlPoint) sourceOrd.resolve(Sys.getStation(), cx).getComponent();
-        if (sourcePoint.getType().equals(BNumericPoint.TYPE) || sourcePoint.getType().equals(BNumericWritable.TYPE)){
-            if(writable){
-                bacnetPointDescriptor = new BBacnetAnalogOutputDescriptor();
-                bacnetPointDescriptor.setObjectId(BBacnetObjectIdentifier.make(1,address)); // 1 is the object type for AnalogOutput
-                ((BBacnetAnalogOutputDescriptor) bacnetPointDescriptor).setBacnetWritable("in10");
+
+        if (sourcePoint.getType().equals(BNumericPoint.TYPE))
+        {
+            bacnetPointDescriptor = createBacnetAnalogValueDescriptor();
+        }
+        else if (sourcePoint.getType().equals(BNumericWritable.TYPE))
+        {
+            if (writable)
+            {
+                bacnetPointDescriptor = createBacnetAnalogOutputDescriptor();
             }
-            else{
-                bacnetPointDescriptor = new BBacnetAnalogValueDescriptor();
-                bacnetPointDescriptor.setObjectId(BBacnetObjectIdentifier.make(2,address)); // 2 is the object type for AnalogValue
-            }
-        } else if (sourcePoint.getType().equals(BBooleanPoint.TYPE) || sourcePoint.getType().equals(BBooleanWritable.TYPE)){
-            if(writable){
-                bacnetPointDescriptor = new BBacnetBinaryOutputDescriptor();
-                bacnetPointDescriptor.setObjectId(BBacnetObjectIdentifier.make(4,address)); // 4 is the object type for BinaryOutput
-                ((BBacnetBinaryOutputDescriptor) bacnetPointDescriptor).setBacnetWritable("in10");
-            }
-            else{
-                bacnetPointDescriptor = new BBacnetBinaryValueDescriptor();
-                bacnetPointDescriptor.setObjectId(BBacnetObjectIdentifier.make(5,address)); // 5 is the object type for BinaryValue
+            else
+            {
+                bacnetPointDescriptor = createBacnetAnalogValueDescriptor();
             }
         }
-        else {
+        else if (sourcePoint.getType().equals(BBooleanPoint.TYPE))
+        {
+            bacnetPointDescriptor = createBacnetBinaryValueDescriptor();
+        }
+        else if (sourcePoint.getType().equals(BBooleanWritable.TYPE))
+        {
+            if (writable)
+            {
+                bacnetPointDescriptor = createBacnetBinaryOutputDescriptor();
+            }
+            else
+            {
+                bacnetPointDescriptor = createBacnetBinaryValueDescriptor();
+            }
+        }
+        else
+        {
             logger.warning("point to export source point type not supported: " + sourcePoint.getType());
             valid = false;
             return null;
@@ -100,8 +141,170 @@ public class PointToExport {
         // set the source ORD
         bacnetPointDescriptor.setPointOrd(sourceOrd);
 
-        bacnetPointDescriptor.setObjectName(pointName+"@"+address);
+        bacnetPointDescriptor.setObjectName(pointName + "@" + address);
 
         return bacnetPointDescriptor;
     }
+
+    /**
+     * Create a Bacnet analog output descriptor.
+     */
+    private BBacnetPointDescriptor createBacnetAnalogOutputDescriptor()
+    {
+        BBacnetPointDescriptor bacnetPointDescriptor;
+        bacnetPointDescriptor = new BBacnetAnalogOutputDescriptor();
+        bacnetPointDescriptor.setObjectId(
+                BBacnetObjectIdentifier.make(1, address)); // 1 is the object type for AnalogOutput
+        ((BBacnetAnalogOutputDescriptor) bacnetPointDescriptor).setBacnetWritable("in10");
+        return bacnetPointDescriptor;
+    }
+
+    /**
+     * Create a Bacnet analog value descriptor.
+     */
+    private BBacnetPointDescriptor createBacnetAnalogValueDescriptor()
+    {
+        BBacnetPointDescriptor bacnetPointDescriptor;
+        bacnetPointDescriptor = new BBacnetAnalogValueDescriptor();
+        bacnetPointDescriptor.setObjectId(
+                BBacnetObjectIdentifier.make(2, address)); // 2 is the object type for AnalogValue
+
+        return bacnetPointDescriptor;
+    }
+
+    /**
+     * Create a Bacnet binary output descriptor.
+     */
+    private BBacnetPointDescriptor createBacnetBinaryOutputDescriptor()
+    {
+        BBacnetPointDescriptor bacnetPointDescriptor;
+        bacnetPointDescriptor = new BBacnetBinaryOutputDescriptor();
+        bacnetPointDescriptor.setObjectId(
+                BBacnetObjectIdentifier.make(4, address)); // 4 is the object type for BinaryOutput
+        ((BBacnetBinaryOutputDescriptor) bacnetPointDescriptor).setBacnetWritable("in10");
+        return bacnetPointDescriptor;
+    }
+
+    /**
+     * Create a Bacnet binary value descriptor.
+     */
+    private BBacnetPointDescriptor createBacnetBinaryValueDescriptor()
+    {
+        BBacnetPointDescriptor bacnetPointDescriptor;
+        bacnetPointDescriptor = new BBacnetBinaryValueDescriptor();
+        bacnetPointDescriptor.setObjectId(
+                BBacnetObjectIdentifier.make(5, address)); // 5 is the object type for BinaryValue
+        return bacnetPointDescriptor;
+    }
+
+    /**
+     * Create a Modbus server control point.
+     */
+    public BControlPoint createModbusServerControlPoint(String pointName, Context cx)
+    {
+        BControlPoint controlPoint;
+        BControlPoint sourcePoint = (BControlPoint) sourceOrd.resolve(Sys.getStation(), cx).getComponent();
+
+        if (sourcePoint.getType().equals(BNumericPoint.TYPE))
+        {
+            controlPoint = createModbusSlaveReadOnlyNumericPoint();
+        }
+        else if (sourcePoint.getType().equals(BNumericWritable.TYPE))
+        {
+            if (writable)
+            {
+                controlPoint = createModbusSlaveWritableNumericPoint();
+            }
+            else
+            {
+                controlPoint = createModbusSlaveReadOnlyNumericPoint();
+            }
+        }
+        else if (sourcePoint.getType().equals(BBooleanPoint.TYPE))
+        {
+            controlPoint = createModbusSlaveReadOnlyBooleanPoint();
+        }
+        else if (sourcePoint.getType().equals(BBooleanWritable.TYPE))
+        {
+            if (writable)
+            {
+                controlPoint = createModbusSlaveWritableBooleanPoint();
+            }
+            else
+            {
+                controlPoint = createModbusSlaveReadOnlyBooleanPoint();
+            }
+        }
+        else
+        {
+            logger.warning("point to export source point type not supported: " + sourcePoint.getType());
+            valid = false;
+            return null;
+        }
+
+        return controlPoint;
+    }
+
+    /**
+     * Create a Modbus slave read only numeric point.
+     */
+    private BControlPoint createModbusSlaveReadOnlyNumericPoint()
+    {
+        BControlPoint controlPoint = new BNumericWritable();
+
+        BModbusServerNumericProxyExt modbusServerNumericProxyExt = new BModbusServerNumericProxyExt();
+        modbusServerNumericProxyExt.setRegType(BRegisterTypeEnum.input);
+        modbusServerNumericProxyExt.setDataType(BDataTypeEnum.integerType);
+
+        controlPoint.setProxyExt(modbusServerNumericProxyExt);
+
+        return controlPoint;
+    }
+
+    /**
+     * Create a Modbus slave writable numeric point.
+     */
+    private BControlPoint createModbusSlaveWritableNumericPoint()
+    {
+        BControlPoint controlPoint = new BNumericWritable();
+
+        BModbusServerNumericProxyExt modbusServerNumericProxyExt = new BModbusServerNumericProxyExt();
+        modbusServerNumericProxyExt.setRegType(BRegisterTypeEnum.holding);
+        modbusServerNumericProxyExt.setDataType(BDataTypeEnum.integerType);
+
+        controlPoint.setProxyExt(modbusServerNumericProxyExt);
+
+        return controlPoint;
+    }
+
+    /**
+     * Create a Modbus slave read only boolean point.
+     */
+    private BControlPoint createModbusSlaveReadOnlyBooleanPoint()
+    {
+        BControlPoint controlPoint = new BBooleanWritable();
+
+        BModbusServerBooleanProxyExt modbusServerBooleanProxyExt = new BModbusServerBooleanProxyExt();
+        modbusServerBooleanProxyExt.setStatusType(BStatusTypeEnum.input);
+
+        controlPoint.setProxyExt(modbusServerBooleanProxyExt);
+
+        return controlPoint;
+    }
+
+    /**
+     * Create a Modbus slave writable boolean point.
+     */
+    private BControlPoint createModbusSlaveWritableBooleanPoint()
+    {
+        BControlPoint controlPoint = new BBooleanWritable();
+
+        BModbusServerBooleanProxyExt modbusServerBooleanProxyExt = new BModbusServerBooleanProxyExt();
+        modbusServerBooleanProxyExt.setStatusType(BStatusTypeEnum.coil);
+
+        controlPoint.setProxyExt(modbusServerBooleanProxyExt);
+
+        return controlPoint;
+    }
+
 }
